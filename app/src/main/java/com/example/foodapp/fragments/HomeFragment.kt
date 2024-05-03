ab74adbaa56bc97abea324fb7b7c68b8f191aeab
baotam546 <donguyenbaotam546@gmail.com>
2024-05-03 16:30:17 +0700

    add button

--- app/src/main/java/com/example/foodapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/foodapp/fragments/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.foodapp.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
+import com.example.foodapp.activity.MealActivity
 import com.example.foodapp.databinding.FragmentHomeBinding
 import com.example.foodapp.pojo.Meal
 import com.example.foodapp.videoModel.HomeViewModel
@@ -36,6 +38,14 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         homeMvvm.getRandomMeal()
         observeRandomMeal()
+        onRandomMealClick()
+    }
+
+    private fun onRandomMealClick() {
+        binding.cardRandomMeal.setOnClickListener{
+            val intent = Intent(activity, MealActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun observeRandomMeal(){
